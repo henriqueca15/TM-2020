095273d6cf536d2c4e7be5757b927279640d5a3c
henriqueca15 <61416626+henriqueca15@users.noreply.github.com>
2020-03-02 16:40:03 +0000

    Henrique Costa 21521

---
+++ Aula 2/exercicio2/HenriqueCosta-21521/script.js
@@ -0,0 +1,17 @@
+var arrayNumbers = [];
+
+function adicionarNumero() {
+    var numero = document.getElementById("numberId").value;
+    arrayNumbers.push(parseInt(numero));
+    document.getElementById( "numerosAdicionados").innerText = arrayNumbers.toString();
+
+}
+
+function  calcularNumero() {
+    if (arrayNumbers.length > 5) {
+        var maiorNumero = Math.max.apply(Math, arrayNumbers);
+        alert(maiorNumero);
+
+    }
+
+}
